821d1c5326e61566dd262d563dff877e7439ed70
Yash <shuklayash1304@gmail.com>
2024-08-17 16:30:23 +0530

    Live Chat

---
+++ src/components/LiveChat.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomNames, makeRandomMessage } from "../utils/helper";
+
+const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
+  const dispatch = useDispatch();
+
+  const chatMessages = useSelector((store) => store.chat.messages);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      // API Polling
+      console.log("API polling");
+      dispatch(
+        addMessage({
+          name: generateRandomNames(),
+          message: makeRandomMessage(25),
+        })
+      );
+    }, 1500);
+
+    return () => clearInterval(i);
+  }, []);
+  return (
+    <div>
+      <div className="ml-2 w-full h-[500px] p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        {/* But never use index as keys */}
+        <div>
+          {chatMessages.map((c, index) => (
+            <ChatMessage key={index} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="w-full p-2 ml-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Yash Shukla",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="w-96 px-2"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default LiveChat;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,5 @@ export const YOUTUBE_VIDEO_API =
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const OFFSET_LIVE_CHAT = 25;
